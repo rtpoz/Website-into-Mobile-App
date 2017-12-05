1784290a340d1fca47fd68dbc2890c893733d123
Md. Sazibur Rahman <sajibrahman.spi@gmail.com>
2017-12-06 00:40:33 +0600

    Website into Mobile App

---
+++ WebsiteConvertApp/app/src/main/java/com/xubisoft/blogapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.xubisoft.blogapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.website);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://jakir.me");
+    }
+}
